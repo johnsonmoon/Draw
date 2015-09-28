5ae482eacfe398e63fe0c845406671c9cfb8f4b8
johnsonmoon <841846248@qq.com>
2015-09-28 17:04:15 +0800

    重写onDraw方法，重写onTouchEvent方法，新增clear方法，新增save以及saveBitmap方法，修改manifest中股阿奴sd卡权限的内容

--- DrawDemo/app/src/main/java/com/xuyihao/drawdemo/DrawView.java
+++ DrawDemo/app/src/main/java/com/xuyihao/drawdemo/DrawView.java
@@ -6,8 +6,15 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Xu Yihao
@@ -37,7 +44,7 @@ public class DrawView extends View{
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(paint.Cap.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeWidth(1);
         paint.setAntiAlias(true);
         paint.setDither(true);
@@ -47,7 +54,69 @@ public class DrawView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawColor(0xFFFFFFFF);//背景颜色
+        Paint bmpPaint = new Paint();//创建画笔
+        canvas.drawBitmap(cacheBitmap, 0, 0, bmpPaint);//绘制cachebitmap
+        canvas.drawPath(path, paint);//绘制路径
+        canvas.save(Canvas.ALL_SAVE_FLAG);//保存canvas状态
+        canvas.restore();//恢复canvas之前保存的状态，防止保存后对canvas执行的操作对后续操作有影响
     }
 
+    //重写onTouchEvent方法
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //获取触摸事件发生的位置
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x, y);//绘图起始点移动至触摸发生位置
+                preX = x;
+                preY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dx = Math.abs(x - preX);
+                float dy = Math.abs(y - preY);
+                if(dx >= 5 || dy >= 5){//判断是否在允许的范围之内
+                    path.quadTo(preX, preY, (x+preX)/2, (y+preY)/2);
+                    preX = x;
+                    preY = y;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                cacheCanvas.drawPath(path, paint);//绘制路径
+                path.reset();
+                break;
+        }
+        invalidate();
+        return true;//表示已经处理该事件
+    }
 
+    //编写clear方法，实现橡皮擦功能
+    public void clear(){
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));//设置图形重叠时候的处理方式
+        paint.setStrokeWidth(50);//设置笔触宽度
+    }
+
+    //编写save方法用以保存当前图片
+    public void save(){
+        try {
+            saveBitmap("myPicture");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //编写saveBitmap方法,保存绘制好的位图
+    public void saveBitmap(String fileName)throws IOException{
+        File file = new File("/sdcard/pictures/"+ fileName +".png");//创建文件对象
+        file.createNewFile();//创建新文件
+        FileOutputStream fileOS = new FileOutputStream(file);//创建文件输出流对象
+
+        //将绘图内容压缩为PNG格式输出至输出流对象
+        cacheBitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOS);
+        fileOS.flush();//将缓冲区的数据全部写出到输出流中
+        fileOS.close();//关闭文件输出流对象
+    }
 }
